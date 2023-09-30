5fe12e4fb1142a874c9130104616ee61ce9b6024
Preston Hager <preston@hagerfamily.com>
2023-09-30 17:13:10 -0600

    SD Card is working

--- src/main.cpp
+++ src/main.cpp
@@ -81,16 +81,10 @@ static void task2(void*) {
     File file;
     while (true) {
         ticktock = !ticktock;
-        if (ticktock) {
-            arduino::Serial.println("TICK");
-            ::vTaskDelay(pdMS_TO_TICKS(1'000));
-        } else {
-            arduino::Serial.println("TOCK");
-            ::vTaskDelay(pdMS_TO_TICKS(1'000));
-        }
-
+        
         // Take the Mutex and write to the file
         // ::xSemaphoreTake(sem, portMAX_DELAY);
+
         file = SD.open("/test.txt", FILE_WRITE);
         if (file) {
             if (ticktock)
@@ -101,8 +95,17 @@ static void task2(void*) {
         } else {
             arduino::Serial.println("error opening test.txt");
         }
+
         // Give back the Mutex
         // ::xSemaphoreGive(sem);
+
+        if (ticktock) {
+            arduino::Serial.println("TICK");
+            ::vTaskDelay(pdMS_TO_TICKS(1'000));
+        } else {
+            arduino::Serial.println("TOCK");
+            ::vTaskDelay(pdMS_TO_TICKS(1'000));
+        }
 #ifdef SDFAT_BASE
         SD.sdfs.end();
 #endif
@@ -123,19 +126,19 @@ FLASHMEM __attribute__((noinline)) void setup() {
 
     ::Serial.println(PSTR("\r\nBooting FreeRTOS kernel " tskKERNEL_VERSION_NUMBER ". Built by gcc " __VERSION__ " (newlib " _NEWLIB_VERSION ") on " __DATE__ ". ***\r\n"));
 
-    // Create a Mutex to act as a lock for the SD card
-    ::SemaphoreHandle_t sem = ::xSemaphoreCreateMutex();
-    // Verify that it was properly created
-    if (sem == NULL) {
-        arduino::Serial.println("Unable to Create Semaphore");
-    } else {
-        ::Serial.println("Counting Semaphore created successfully");
-        while(1);
-    }
+    // // Create a Mutex to act as a lock for the SD card
+    // ::SemaphoreHandle_t sem = ::xSemaphoreCreateMutex();
+    // // Verify that it was properly created
+    // if (sem == NULL) {
+    //     arduino::Serial.println("Unable to Create Semaphore");
+    // } else {
+    //     ::Serial.println("Counting Semaphore created successfully");
+    //     while(1);
+    // }
 
     // Create tasks, and pass the semaphore into them
-    ::xTaskCreate(task1, "task1", 128, (void*)&sem, 2, nullptr);
-    ::xTaskCreate(task2, "task2", 2048, (void*)&sem, 2, nullptr);
+    ::xTaskCreate(task1, "task1", 128, nullptr, 2, nullptr);
+    ::xTaskCreate(task2, "task2", 2048, nullptr, 2, nullptr);
 
     ::Serial.println("setup(): starting scheduler...");
     ::Serial.flush();
